c5e3b01a5657f382592064236a25e83f4feb2e76
BeckHelloWord <444870506@qq.com>
2019-03-17 16:46:06 +0800

    菜单路由

--- src/components/aside/index.vue
+++ src/components/aside/index.vue
@@ -1,20 +1,20 @@
 <template>
-  <el-aside class="aside" :style="{width:isCollapse?'65px':'300px'}">
-    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-      <el-radio-button :label="false">展开</el-radio-button>
-      <el-radio-button :label="true">收起</el-radio-button>
-    </el-radio-group>
-    <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="isCollapse">
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
+  <el-aside class="aside" :style="{width:setWidth}">
+    <div style="text-align:right;">
+      <i class="el-icon-arrow-left" v-show="!isCollapse" @click="isCollapse=true"></i>
+      <i class="el-icon-arrow-right" v-show="isCollapse" @click="isCollapse=false"></i>
+    </div>
+    <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="isCollapse" :router="true">
+      <el-menu-item index="2" :route="{path: '/'}">
+        <i class="iconfont">&#xe65e;</i>
         <span slot="title">主页</span>
       </el-menu-item>
       <el-submenu index="2">
         <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
+          <i class="iconfont">&#xe616;</i>
+          <span slot="title">用户管理</span>
         </template>
-        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-1" :route="{path: '/user/userList'}">全部用户</el-menu-item>
         <el-menu-item index="1-2">选项2</el-menu-item>
       </el-submenu>
       <el-menu-item index="3" disabled>
@@ -32,6 +32,13 @@
 export default {
   data() {
     return { isCollapse: true };
+  },
+  computed: {
+    setWidth: function() {
+      this.$nextTick(() => {
+        return this.isCollapse ? "65px" : "300px";
+      });
+    }
   },
   methods: {}
 };
